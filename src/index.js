bf34acf2eaf6633522af9e202dc9b51c258c5741
Ihar Vypau <igor.vypov@gmail.com>
2020-10-04 14:17:19 +0300

    feat:implement morse decoder task

--- src/index.js
+++ src/index.js
@@ -1,44 +1,62 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    let messageConverted = [];
+    let getSubArraysFromArray = (arr, size) => {
+        let subArray = [];
+        for (let i = 0; i < expr.length; i += size) {
+            subArray.push(arr.slice(i, i + size));
+        }
+        return subArray;
+    }
+    expr.split("**********").map((word) => {
+        let lettersArray = getSubArraysFromArray(word.split(''), 10);
+        let decodedWord = lettersArray.map((letter) => {
+            let morseKey = letter.join('').replace(/10/g, '.').replace(/11/g, '-').replace(/0+/, '');
+            return MORSE_TABLE[morseKey];
+        })
+        messageConverted.push(decodedWord.join(''));
+    });
+
+    console.log(messageConverted.join(" "));
+    return messageConverted.join(" ");
 }
 
 module.exports = {
